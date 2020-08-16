2e2e0d6d4a665ac457a65d3199478fbe057ce98b
Lucas Suzigan <50274831+lsuzigan@users.noreply.github.com>
2020-08-16 12:49:13 -0300

    Starting configuring data access

---
+++ DataAccessLibrary/IDataAccess.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DataAccessLibrary
+{
+    public interface IDataAccess
+    {
+        List<T> LoadData<T, U>(string sql, U parameters, string connectionString);
+        void SaveData<T>(string sql, T parameters, string connectionString);
+    }
+}
