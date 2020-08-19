97fe399ffaf4539e31174727fa713c8a3340ae40
Lucas Suzigan <50274831+lsuzigan@users.noreply.github.com>
2020-08-19 00:41:54 -0300

    CreateClient page added
    Can't create a client yet

--- BikeShop/Models/Client.cs
+++ BikeShop/Models/Client.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MySqlX.XDevAPI.Relational;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccessLibrary;
 
 namespace BikeShop.Models
 {
@@ -9,8 +11,18 @@ namespace BikeShop.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Cpf { get; set; }
+        public string Document { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+
+        public async Task CreateNewClient(Client newClient)
+        {
+            DataAccess _db = new DataAccess();
+
+            string sql = @"insert into `bikeshop`.`client` (`name`, `document`, `phone_number`, `email`)
+                            values (@newClient.Name, @newClient.Document, @newClient.PhoneNumber, @newClient.Email);";
+
+            await _db.SaveData(sql, newClient, "default");
+        }
     }
 }
